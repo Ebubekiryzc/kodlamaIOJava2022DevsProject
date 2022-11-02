830a8c09029fdeda0aa15afe609e0567b5911807
ebubekiryzc <ebubekiryazi@gmail.com>
2022-11-02 11:35:42 +0300

    Created custom requests for programming language entity

---
+++ src/main/java/kodlama/io/devs/webapi/requests/programminglanguage/ProgrammingLanguageCreateRequest.java
@@ -0,0 +1,8 @@
+package kodlama.io.devs.webapi.requests.programminglanguage;
+
+import lombok.Data;
+
+@Data
+public class ProgrammingLanguageCreateRequest {
+    private String name;
+}
---
+++ src/main/java/kodlama/io/devs/webapi/requests/programminglanguage/ProgrammingLanguageUpdateRequest.java
@@ -0,0 +1,9 @@
+package kodlama.io.devs.webapi.requests.programminglanguage;
+
+import lombok.Data;
+
+@Data
+public class ProgrammingLanguageUpdateRequest {
+    private Long id;
+    private String name;
+}
