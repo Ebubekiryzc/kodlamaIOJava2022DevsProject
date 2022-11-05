6606f6da7e76da5de01577fe72b911a1a21867b8
ebubekiryzc <ebubekiryazi@gmail.com>
2022-11-05 16:44:53 +0300

    Added new business rule for getting programming language by id

--- src/main/java/kodlama/io/devs/business/rules/ProgrammingLanguageBusinessRules.java
+++ src/main/java/kodlama/io/devs/business/rules/ProgrammingLanguageBusinessRules.java
@@ -2,22 +2,28 @@ package kodlama.io.devs.business.rules;
 
 import kodlama.io.devs.dataaccess.abstracts.ProgrammingLanguageRepository;
 import kodlama.io.devs.entities.concretes.ProgrammingLanguage;
-import org.springframework.beans.factory.annotation.Autowired;
+import kodlama.io.devs.exception.classes.ProgrammingLanguageNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class ProgrammingLanguageBusinessRules {
 
-    @Autowired
-    private ProgrammingLanguageRepository programmingLanguageRepository;
+    private final ProgrammingLanguageRepository programmingLanguageRepository;
 
     public void programmingLanguageNameMustBeUniqueWhenCreating(ProgrammingLanguage programmingLanguage) {
-        ProgrammingLanguage existence = programmingLanguageRepository.getAll().stream().filter(p -> p.getName().equalsIgnoreCase(programmingLanguage.getName())).findAny().orElse(null);
+        ProgrammingLanguage existence = programmingLanguageRepository.findAll().stream().filter(p -> p.getName().equalsIgnoreCase(programmingLanguage.getName())).findAny().orElse(null);
         if (existence != null) throw new RuntimeException("Programming Language name exist.");
     }
 
     public void programmingLanguageNameMustBeUniqueWhenUpdating(ProgrammingLanguage programmingLanguage) {
-        ProgrammingLanguage existence = programmingLanguageRepository.getAll().stream().filter(p -> p.getName().equalsIgnoreCase(programmingLanguage.getName()) && p.getId() != programmingLanguage.getId()).findAny().orElse(null);
+        ProgrammingLanguage existence = programmingLanguageRepository.findAll().stream().filter(p -> p.getName().equalsIgnoreCase(programmingLanguage.getName()) && p.getId() != programmingLanguage.getId()).findAny().orElse(null);
         if (existence != null) throw new RuntimeException("Programming Language name exist.");
+    }
+
+    public void programmingLanguageMustExistWhenRequested(Long id) {
+        programmingLanguageRepository
+                .findById(id).orElseThrow(() -> new ProgrammingLanguageNotFoundException("Requested programming language does not exist."));
     }
 }
