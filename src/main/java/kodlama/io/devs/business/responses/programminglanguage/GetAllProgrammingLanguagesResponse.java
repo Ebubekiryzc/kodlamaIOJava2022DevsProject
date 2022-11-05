2c51e5816648e8beb19257ef5758f48a46b20a3d
ebubekiryzc <ebubekiryazi@gmail.com>
2022-11-05 16:46:18 +0300

    Create new responses programming language requests

---
+++ src/main/java/kodlama/io/devs/business/responses/programminglanguage/GetAllProgrammingLanguagesResponse.java
@@ -0,0 +1,9 @@
+package kodlama.io.devs.business.responses.programminglanguage;
+
+import lombok.Data;
+
+@Data
+public class GetAllProgrammingLanguagesResponse {
+    private Long id;
+    private String name;
+}
