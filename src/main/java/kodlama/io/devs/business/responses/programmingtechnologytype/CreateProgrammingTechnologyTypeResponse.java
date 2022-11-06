ff252cdf5d48d650f3c27e78eb2d7649b8b300b6
ebubekiryzc <ebubekiryazi@gmail.com>
2022-11-06 22:29:40 +0300

    Created custom responses for programming technology type entity

---
+++ src/main/java/kodlama/io/devs/business/responses/programmingtechnologytype/CreateProgrammingTechnologyTypeResponse.java
@@ -0,0 +1,9 @@
+package kodlama.io.devs.business.responses.programmingtechnologytype;
+
+import lombok.Data;
+
+@Data
+public class CreateProgrammingTechnologyTypeResponse {
+    private Long id;
+    private String name;
+}
