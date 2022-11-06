c7f7367024d14d8e623478f590047304cd70d548
ebubekiryzc <ebubekiryazi@gmail.com>
2022-11-06 23:35:59 +0300

    Refactored for newly added entity mappings

--- src/main/java/kodlama/io/devs/business/responses/programminglanguage/GetAllProgrammingLanguagesResponse.java
+++ src/main/java/kodlama/io/devs/business/responses/programminglanguage/GetAllProgrammingLanguagesResponse.java
@@ -1,9 +1,14 @@
 package kodlama.io.devs.business.responses.programminglanguage;
 
+import kodlama.io.devs.entities.concretes.ProgrammingTechnology;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class GetAllProgrammingLanguagesResponse {
     private Long id;
     private String name;
+    private List<ProgrammingTechnology> programmingTechnologies;
+
 }
