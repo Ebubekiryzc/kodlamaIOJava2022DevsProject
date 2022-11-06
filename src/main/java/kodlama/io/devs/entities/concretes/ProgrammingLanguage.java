5e81683efc81e3ff4073ba6f59ca22d8d2089b8d
ebubekiryzc <ebubekiryazi@gmail.com>
2022-11-06 23:35:11 +0300

    Refactored for newly added entity mappings

--- src/main/java/kodlama/io/devs/entities/concretes/ProgrammingLanguage.java
+++ src/main/java/kodlama/io/devs/entities/concretes/ProgrammingLanguage.java
@@ -1,10 +1,12 @@
 package kodlama.io.devs.entities.concretes;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -15,5 +17,12 @@ public class ProgrammingLanguage {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
+    @OneToMany(cascade = CascadeType.ALL,
+            mappedBy = "programmingLanguage",
+            fetch = FetchType.LAZY)
+    @JsonManagedReference
+    private List<ProgrammingTechnology> programmingTechnologies;
 }
