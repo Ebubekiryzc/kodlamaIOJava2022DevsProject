874b9364223362bb8e57b1c013035035053ff495
ebubekiryzc <ebubekiryazi@gmail.com>
2022-11-06 22:29:27 +0300

    Created custom requests for programming technology type entity

---
+++ src/main/java/kodlama/io/devs/business/requests/programmingtechnologytype/UpdateProgrammingTechnologyTypeRequest.java
@@ -0,0 +1,9 @@
+package kodlama.io.devs.business.requests.programmingtechnologytype;
+
+import lombok.Data;
+
+@Data
+public class UpdateProgrammingTechnologyTypeRequest {
+    private Long id;
+    private String name;
+}
